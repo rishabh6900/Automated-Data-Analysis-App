4db011ebaea41dd82dd55a0f2328bf5a7fbc1bec
Rishabh Prajapati <prajapatirishabh071@gmail.com>
2025-03-22 14:51:32 +0530

    idaa

--- IDAA.py
+++ IDAA.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 import pandas_profiling
-from ydata_profiling import ProfileReport
+#from ydata_profiling import ProfileReport
 
 
 
@@ -129,7 +129,8 @@ def main():
         try:
             # Pandas Profiling Report
             st.write("## Detailed Data Report")
-            profile = ydata_profiling.ProfileReport(df)
+            #profile = ydata_profiling.ProfileReport(df)
+            profile = pandas_profiling.ProfileReport(df)
             st_profile_report(profile)
             st.success("Pandas profiling report successfully generated!")
         except Exception as e:
